34ea939d0885bda0891b056a152c076d95facb05
wadeintheweb <wade@wadeintheweb.ca>
2023-05-11 17:28:41 -0600

    first commit

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
-
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
